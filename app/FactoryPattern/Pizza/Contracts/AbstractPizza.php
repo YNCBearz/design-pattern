7520a5dc30ba65f2a67de3d4b1792c22105b0b6d
YNCBearz <bear19930414@gmail.com>
2020-03-29 20:21:11 +0800

    修改披薩原型

--- app/FactoryPattern/Pizza/Contracts/AbstractPizza.php
+++ app/FactoryPattern/Pizza/Contracts/AbstractPizza.php
@@ -6,25 +6,59 @@ use App\FactoryPattern\Pizza\Contracts\PizzaInterface;
 
 abstract class AbstractPizza implements PizzaInterface
 {
+    /**
+     * @var string $name 披薩名稱
+     * @var string $dough 麵團類型
+     * @var string $sauce 醬料類型
+     * @var array $toppings 佐料
+     */
     public $name;
+    public $dough = '普通麵團';
+    public $sauce = '一般醬料';
+    public $toppings = [];
 
     public function prepare()
     {
-        echo "$this->name 準備中 \n";
+        echo "{$this->name} 準備中 \n";
+        echo "捏揉{$this->dough}... \n";
+        echo "加入{$this->sauce}... \n";
+
+        $this->addToppings();
+    }
+
+    private function noToppings()
+    {
+        return count($this->toppings) == 0;
+    }
+
+    private function addToppings()
+    {
+        $toppings = $this->toppings;
+
+        if ($this->noToppings()) {
+            return;
+        }
+
+
+        echo "加入佐料: \n";
+
+        foreach ($toppings as $key => $topping) {
+            echo "      $topping \n";
+        }
     }
 
     public function bake()
     {
-        echo "$this->name 烘烤中 \n";
+        echo "以溫度350度烘烤25分鐘 \n";
     }
 
     public function cut()
     {
-        echo "$this->name 切片中 \n";
+        echo "將披薩切片 \n";
     }
 
     public function box()
     {
-        echo "$this->name 裝箱中 \n";
+        echo "裝進官方的披薩盒 \n";
     }
 }
