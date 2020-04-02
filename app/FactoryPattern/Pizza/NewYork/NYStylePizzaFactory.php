3aa1417c3483c4e70849fa616556144779e89d9c
YNCBearz <bear19930414@gmail.com>
2020-04-03 01:00:20 +0800

    實作紐約原料工廠

--- app/FactoryPattern/Pizza/NewYork/NYStylePizzaFactory.php
+++ app/FactoryPattern/Pizza/NewYork/NYStylePizzaFactory.php
@@ -1,19 +1,28 @@
 <?php
 namespace App\FactoryPattern\Pizza\NewYork;
 
+use App\FactoryPattern\Pizza\ClamPizza;
+use App\FactoryPattern\Pizza\CheesePizza;
 use App\FactoryPattern\Pizza\Contracts\AbstractPizza as Pizza;
+use App\FactoryPattern\Pizza\NewYork\NYStyleIngredientFactory;
 
 class NYStylePizzaFactory
 {
     public function createPizza($type): Pizza
     {
+        $ingredientFactory = new NYStyleIngredientFactory();
+
         switch ($type) {
             case 'cheese':
-                $pizza = new NYStyleCheesePizza();
+                $pizza = new CheesePizza($ingredientFactory);
+                $pizza->name = '紐約風起司披薩';
+                $pizza->toppings = ['義大利高級乾酪'];
                 break;
 
             case 'clam':
-                $pizza = new NYStyleClamPizza();
+                $pizza = new ClamPizza($ingredientFactory);
+                $pizza->name = '紐約風蛤蜊披薩';
+                $pizza->toppings = ['龍蝦蟹肉棒'];
                 break;
         }
 
