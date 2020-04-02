9f704d8e5b88a1a054e32141a89ab582af8f0357
YNCBearz <bear19930414@gmail.com>
2020-04-03 00:57:34 +0800

    實作創始店的原料工廠

--- app/FactoryPattern/Pizza/SimplePizzaFactory.php
+++ app/FactoryPattern/Pizza/SimplePizzaFactory.php
@@ -4,23 +4,29 @@ namespace App\FactoryPattern\Pizza;
 use App\FactoryPattern\Pizza\ClamPizza;
 use App\FactoryPattern\Pizza\CheesePizza;
 use App\FactoryPattern\Pizza\PepperoniPizza;
+use App\FactoryPattern\Pizza\FounderIngredientFactory;
 use App\FactoryPattern\Pizza\Contracts\AbstractPizza as Pizza;
 
 class SimplePizzaFactory
 {
     public function createPizza($type): Pizza
     {
+        $ingredientFactory = new FounderIngredientFactory();
+
         switch ($type) {
             case 'cheese':
-                $pizza = new CheesePizza();
+                $pizza = new CheesePizza($ingredientFactory);
+                $pizza->name = '起司披薩';
                 break;
 
             case 'pepperoni':
-                $pizza = new PepperoniPizza();
+                $pizza = new PepperoniPizza($ingredientFactory);
+                $pizza->name = '義式臘腸披薩';
                 break;
 
             case 'clam':
-                $pizza = new ClamPizza();
+                $pizza = new ClamPizza($ingredientFactory);
+                $pizza->name = '蛤蜊披薩';
                 break;
         }
 
