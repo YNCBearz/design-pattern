ad64b41fb90d7ab6c51427ef32a71d6c8e787f00
YNCBearz <bear19930414@gmail.com>
2020-04-19 00:53:15 +0800

    新增解決鍋爐衝突的需求

--- app/SingletonPattern/ChocolateFactory/ChocolateFactory.php
+++ app/SingletonPattern/ChocolateFactory/ChocolateFactory.php
@@ -7,7 +7,15 @@ class ChocolateFactory
 {
     public function run()
     {
+        //員工A上班，開始煮巧克力
         $boiler = new ChocolateBoiler();
+
+        $boiler->fill();
+        $boiler->boil();
+
+        //員工B上班，開始煮巧克力
+        $boiler = new ChocolateBoiler();
+        $boiler->drain();
 
         $boiler->fill();
         $boiler->boil();
