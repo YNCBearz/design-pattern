70836afb09cf2ce6e356e545b5c79b22e9ac18ee
YNCBearz <bear19930414@gmail.com>
2020-07-19 20:49:33 +0800

    完成新增CD播放的遙控器

--- app/CommandPattern/RemoteControl/RemoteControl.php
+++ app/CommandPattern/RemoteControl/RemoteControl.php
@@ -1,45 +1,56 @@
 <?php
+
 namespace App\CommandPattern\RemoteControl;
 
 use App\CommandPattern\RemoteControl\Contracts\Command;
 use App\CommandPattern\RemoteControl\Receiver\Light;
 use App\CommandPattern\RemoteControl\LightOnCommand;
 use App\CommandPattern\RemoteControl\NoCommand;
+use App\CommandPattern\RemoteControl\Receiver\CDPlayer;
+use App\CommandPattern\RemoteControl\CDPlayerOnCommand;
+use App\CommandPattern\RemoteControl\CDPlayerOffCommand;
 
 class RemoteControl
 {
     /**
-     * @var Command
+     * @var int
+     */
+    protected $slotAmount = 2;
+
+    /**
+     * @var Command[]
      */
     protected $onCommand;
 
     /**
-     * @var Command
+     * @var Command[]
      */
     protected $offCommand;
 
     public function __construct()
     {
-        $this->onCommand = new NoCommand();
-        $this->offCommand = new NoCommand();
+        for ($slotNumber = 0; $slotNumber < $this->slotAmount; $slotNumber++) {
+            $this->onCommand[$slotNumber] = new NoCommand();
+            $this->offCommand[$slotNumber] = new NoCommand();
+        }
     }
 
-    public function setCommand(Command $onCommand, Command $offCommand)
+    public function setCommand(int $slotNumber, Command $onCommand, Command $offCommand)
     {
-        $this->onCommand = $onCommand;
-        $this->offCommand = $offCommand;
+        $this->onCommand[$slotNumber] = $onCommand;
+        $this->offCommand[$slotNumber] = $offCommand;
     }
 
-    public function onButtonWasPressed()
+    public function onButtonWasPressed(int $slotNumber)
     {
-        echo "按下按扭 \n";
-        $this->onCommand->execute();
+        echo "按下開啟按扭$slotNumber \n";
+        $this->onCommand[$slotNumber]->execute();
     }
 
-    public function offButtonWasPressed()
+    public function offButtonWasPressed(int $slotNumber)
     {
-        echo "按下按扭 \n";
-        $this->offCommand->execute();
+        echo "按下關閉按扭$slotNumber \n";
+        $this->offCommand[$slotNumber]->execute();
     }
 
     public function run()
@@ -48,8 +59,19 @@ class RemoteControl
         $lightOnCommand = new LightOnCommand($light);
         $lightOffCommand = new LightOffCommand($light);
 
-        $this->setCommand($lightOnCommand, $lightOffCommand);
-        $this->onButtonWasPressed();
-        $this->offButtonWasPressed();
+        $this->setCommand(0, $lightOnCommand, $lightOffCommand);
+        $this->onButtonWasPressed(0);
+        $this->offButtonWasPressed(0);
+
+        $this->onButtonWasPressed(1);
+        $this->offButtonWasPressed(1);
+
+        $cdPlayer = new CDPlayer();
+        $cdPlayerOnCommand = new CDPlayerOnCommand($cdPlayer);
+        $cdPlayerOffCommand = new CDPlayerOffCommand($cdPlayer);
+
+        $this->setCommand(1, $cdPlayerOnCommand, $cdPlayerOffCommand);
+        $this->onButtonWasPressed(1);
+        $this->offButtonWasPressed(1);
     }
 }
