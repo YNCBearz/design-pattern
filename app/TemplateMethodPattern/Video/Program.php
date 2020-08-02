2f0d2095d1f2642390c2c973b6aa7a8c19122a28
YNCBearz <bear19930414@gmail.com>
2020-08-02 23:25:23 +0800

    改版

--- app/TemplateMethodPattern/Video/Program.php
+++ app/TemplateMethodPattern/Video/Program.php
@@ -2,35 +2,24 @@
 
 namespace App\TemplateMethodPattern\Video;
 
+use App\TemplateMethodPattern\Video\UnboxVideo;
+
 class Program
 {
     public function makeUnboxVideo()
     {
-        return "
-            選擇開箱項目 \n
-            拍攝 \n
-            剪輯出個人開箱的風格 \n
-            上傳影片 \n
-        ";
+        // $unboxVideo = new UnboxVideo();
+        // return $unboxVideo->make();
+        return '選擇開箱項目、拍攝、剪輯出個人開箱的風格、上傳影片';
     }
 
     public function makeTutorialVideo()
     {
-        return "
-            設計教學內容 \n
-            拍攝 \n
-            剪輯出個人教學的風格 \n
-            上傳影片 \n
-        ";
+        return '設計教學內容、拍攝、剪輯出個人教學的風格、上傳影片';
     }
 
     public function makeStoryVideo()
     {
-        return "
-            選擇故事主題 \n
-            拍攝 \n
-            剪輯出個人說故事的風格 \n
-            上傳影片 \n
-        ";
+        return '選擇故事主題、拍攝、剪輯出個人說故事的風格、上傳影片';
     }
 }
--- tests/Unit/TemplateMethodPattern/Video/ProgramTest.php
+++ tests/Unit/TemplateMethodPattern/Video/ProgramTest.php
@@ -20,12 +20,7 @@ class ProgramTest extends TestCase
 
     public function testMakeUnboxVideo()
     {
-        $expected = "
-            選擇開箱項目 \n
-            拍攝 \n
-            剪輯出個人開箱的風格 \n
-            上傳影片 \n
-        ";
+        $expected = '選擇開箱項目、拍攝、剪輯出個人開箱的風格、上傳影片';
 
         $actual = $this->sut->makeUnboxVideo();
         $this->assertEquals($expected, $actual);
@@ -33,12 +28,7 @@ class ProgramTest extends TestCase
 
     public function testMakeTutorialVideo()
     {
-        $expected = "
-            設計教學內容 \n
-            拍攝 \n
-            剪輯出個人教學的風格 \n
-            上傳影片 \n
-        ";
+        $expected = '設計教學內容、拍攝、剪輯出個人教學的風格、上傳影片';
 
         $actual = $this->sut->makeTutorialVideo();
         $this->assertEquals($expected, $actual);
@@ -46,12 +36,7 @@ class ProgramTest extends TestCase
 
     public function testMakeStoryVideo()
     {
-        $expected = "
-            選擇故事主題 \n
-            拍攝 \n
-            剪輯出個人說故事的風格 \n
-            上傳影片 \n
-        ";
+        $expected = '選擇故事主題、拍攝、剪輯出個人說故事的風格、上傳影片';
 
         $actual = $this->sut->makeStoryVideo();
         $this->assertEquals($expected, $actual);
