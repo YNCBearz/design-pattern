67151b140b298701fb914f618b8b259365f67dd5
YNCBearz <bear19930414@gmail.com>
2020-08-09 16:07:07 +0800

    改寫Program

--- app/AdapterPattern/TalentShow/Program.php
+++ app/AdapterPattern/TalentShow/Program.php
@@ -4,27 +4,36 @@ namespace App\AdapterPattern\TalentShow;
 
 use App\AdapterPattern\TalentShow\Adapter\PianoPlayerAdapter;
 use App\AdapterPattern\TalentShow\Adapter\JokeTellerAdapter;
+use App\AdapterPattern\TalentShow\Contracts\ShowInterface;
 
 class Program
 {
+    /**
+     * @var ShowInterface[]
+     */
+    protected $performers = [];
+
     /**
      * @return array
      */
     public function run()
     {
-        $performers = [];
-
-        $pianoPlayer = new PianoPlayer();
-        $performers[] = new PianoPlayerAdapter($pianoPlayer);
-
-        $jokeTeller = new JokeTeller();
-        $performers[] = new JokeTellerAdapter($jokeTeller);
+        $this->preparePerformers();
 
         $result = [];
-        foreach ($performers as $performer) {
+        foreach ($this->performers as $performer) {
             $result[] = $performer->show();
         }
 
         return $result;
+    }
+
+    private function preparePerformers()
+    {
+        $pianoPlayer = new PianoPlayer();
+        $this->performers[] = new PianoPlayerAdapter($pianoPlayer);
+
+        $jokeTeller = new JokeTeller();
+        $this->performers[] = new JokeTellerAdapter($jokeTeller);
     }
 }
