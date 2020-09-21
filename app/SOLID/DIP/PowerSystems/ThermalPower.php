a15bc7489076b5a70f6af7a33aa8792e7580a628
YNCBearz <bear19930414@gmail.com>
2020-09-21 11:41:56 +0800

    update

--- app/SOLID/DIP/PowerSystems/ThermalPower.php
+++ app/SOLID/DIP/PowerSystems/ThermalPower.php
@@ -6,8 +6,6 @@ use App\SOLID\DIP\PowerSystems\Contracts\PowerGeneratable;
 
 class ThermalPower implements PowerGeneratable
 {
-    protected $name = '火力發電';
-
     public function generatePower()
     {
         return '電力';
