1b677c2213ad33700b3f43db850aa136c4792539
YNCBearz <bear19930414@gmail.com>
2020-09-21 11:36:46 +0800

    WindPower

--- tests/Feature/SOLID/DIP/PowerSystems/ProgramTest.php
+++ tests/Feature/SOLID/DIP/PowerSystems/ProgramTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\SOLID\DIP\PowerSystems;
 use PHPUnit\Framework\TestCase;
 use App\SOLID\DIP\PowerSystems\Program;
 use App\SOLID\DIP\PowerSystems\ThermalPower;
+use App\SOLID\DIP\PowerSystems\WindPower;
 
 class ProgramTest extends TestCase
 {
@@ -22,6 +23,14 @@ class ProgramTest extends TestCase
     {
         $expected = '電力';
         $powerGeneration = new ThermalPower();
+        $actual = $this->sut->getPower($powerGeneration);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testGetPowerByWindPower()
+    {
+        $expected = '電力';
+        $powerGeneration = new WindPower();
         $actual = $this->sut->getPower($powerGeneration);
         $this->assertEquals($expected, $actual);
     }
