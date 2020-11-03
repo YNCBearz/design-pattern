9dd342d13d658e11ae4693d25cd8925b00dd1c89
Bear <bear.hsiung@drsays.org>
2020-11-03 11:15:01 +0800

    refactor

--- app/InterpreterPattern/MorseCode/NonTerminalExpression.php
+++ app/InterpreterPattern/MorseCode/NonTerminalExpression.php
@@ -2,14 +2,25 @@
 
 namespace App\InterpreterPattern\MorseCode;
 
-use App\InterpreterPattern\MorseCode\Abstracts\Expression;
+use App\InterpreterPattern\MorseCode\Contracts\Expression;
+use App\InterpreterPattern\MorseCode\Context;
 
-class NonTerminalExpression extends Expression
+class NonTerminalExpression implements Expression
 {
+    public function interpret(Context $context): Context
+    {
+        $head = ' ';
+        $context->text = trim($context->text);
+
+        $this->execute($head);
+        return $context;
+    }
+
+
     /**
-     * @param string $key
+     * @param string $message
      */
-    public function execute(string $key)
+    public function execute(string $message)
     {
         echo ' / ';
     }
