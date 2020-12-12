3eebb124f59d3e241c32e65f20aab7a3b0028ce4
YNCBearz <bear19930414@gmail.com>
2020-12-12 23:00:06 +0800

    update

--- app/ChainOfResponsibilityPattern/Software/Request.php
+++ app/ChainOfResponsibilityPattern/Software/Request.php
@@ -14,17 +14,27 @@ class Request
      */
     protected $content;
 
+    /**
+     * @param string $type
+     * @param string $content
+     */
     public function __construct(string $type, string $content)
     {
         $this->type = $type;
         $this->content = $content;
     }
 
+    /**
+     * @return string
+     */
     public function getType()
     {
         return $this->type;
     }
 
+    /**
+     * @return string
+     */
     public function getContent()
     {
         return $this->content;
