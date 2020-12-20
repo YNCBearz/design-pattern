67093060b80bb994798bda1dfa49e1caea65e1ae
YNCBearz <bear19930414@gmail.com>
2020-12-20 15:12:36 +0800

    refactor

--- app/MediatorPattern/SayHello/Program.php
+++ app/MediatorPattern/SayHello/Program.php
@@ -4,21 +4,58 @@ namespace App\MediatorPattern\SayHello;
 
 class Program
 {
-    public function sayHello()
+
+    /**
+     * @param string $item
+     * @return string
+     */
+    public function see($item)
     {
-        //看到認識的人
+        switch ($item) {
+            case '認識的人':
+                return $this->sayHello();
+                break;
+
+            case '熟識的人':
+                return $this->waveHand();
+                break;
+        }
+    }
+
+    /**
+     * @param string $item
+     * @return string
+     */
+    public function hear($item)
+    {
+        switch ($item) {
+            case '喜歡的人':
+                return $this->blush();
+                break;
+
+            case '討厭的人':
+                return $this->pretendToLookBusy();
+                break;
+        }
+    }
+
+    private function sayHello()
+    {
         return '[嘴巴]發出[你好]的聲音';
     }
 
-    public function waveHand()
+    private function waveHand()
     {
-        //看到熟識的人
         return '[手]做出[揮手]的動作';
     }
 
-    public function blush()
+    private function blush()
     {
-        //看到喜歡的人
         return '[臉]開始[發紅]';
+    }
+
+    private function pretendToLookBusy()
+    {
+        return '[手]做出[裝忙]的動作';
     }
 }
--- tests/Unit/MediatorPattern/SayHello/ProgramTest.php
+++ tests/Unit/MediatorPattern/SayHello/ProgramTest.php
@@ -18,24 +18,43 @@ class ProgramTest extends TestCase
         $this->sut = new Program();
     }
 
-    public function testSayHello()
+    /**
+     * @test
+     */
+    public function Given_People_You_Know_When_See_Then_Say_Hello()
     {
         $expected = '[嘴巴]發出[你好]的聲音';
-        $actual = $this->sut->sayHello();
+        $actual = $this->sut->see('認識的人');
         $this->assertEquals($expected, $actual);
     }
 
-    public function testWaveHand()
+    /**
+     * @test
+     */
+    public function Given_Friend_When_See_Then_Wave_Hand()
     {
         $expected = '[手]做出[揮手]的動作';
-        $actual = $this->sut->waveHand();
+        $actual = $this->sut->see('熟識的人');
         $this->assertEquals($expected, $actual);
     }
 
-    public function testBlush()
+    /**
+     * @test
+     */
+    public function Given_Lover_When_Hear_Then_Blush()
     {
         $expected = '[臉]開始[發紅]';
-        $actual = $this->sut->blush();
+        $actual = $this->sut->hear('喜歡的人');
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function Given_Asshole_When_Hear_Then_Pretend_To_Look_Busy()
+    {
+        $expected = '[手]做出[裝忙]的動作';
+        $actual = $this->sut->hear('討厭的人');
         $this->assertEquals($expected, $actual);
     }
 }
